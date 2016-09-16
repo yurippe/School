046d4edbbe9f085661eb30b987d38f2061072613
yurippe <yurippenet@gmail.com>
2016-09-16 12:53:52 +0200

    fixed question 2

--- dDatabases/Week 2/Answers.sql
+++ dDatabases/Week 2/Answers.sql
@@ -4,7 +4,7 @@ SELECT DISTINCT name FROM People INNER JOIN Takes ON People.pid = Takes.pid WHER
 #################################################################################################################
 #Which teachers have taught the same course?
 #NB NOT FULL SOLUTION (Duplicate tuples, like (A, B) and (B, A) both gets listed)
-SELECT * FROM (SELECT People.pid, cid, People.name FROM Teaches INNER JOIN People ON Teaches.pid = People.pid) as p1 INNER JOIN (SELECT People.pid, cid, People.name FROM Teaches INNER JOIN People ON Teaches.pid = People.pid) as p2 WHERE p1.pid <> p2.pid AND p1.cid = p2.cid;
+SELECT p1.name as Teacher1, p2.name as Teacher2 FROM (SELECT People.pid, cid, People.name FROM Teaches INNER JOIN People ON Teaches.pid = People.pid) as p1 INNER JOIN (SELECT People.pid, cid, People.name FROM Teaches INNER JOIN People ON Teaches.pid = People.pid) as p2 WHERE p1.pid < p2.pid AND p1.cid = p2.cid;
 #################################################################################################################
 #List each student together with the average grade for the student across all courses the student has taken
 #NB NOT FULL SOLUTION (counts duplicates of same course if the person has previously failed the course)
