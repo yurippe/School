b46d59a37ac02aec0f23ae9469e2563e82ec1956
yurippe <yurippenet@gmail.com>
2016-09-16 13:30:44 +0200

    All answers now works

--- dDatabases/Week 2/Answers.sql
+++ dDatabases/Week 2/Answers.sql
@@ -3,13 +3,24 @@
 SELECT DISTINCT name FROM People INNER JOIN Takes ON People.pid = Takes.pid WHERE Takes.grade <= 10;
 #################################################################################################################
 #Which teachers have taught the same course?
-#NB NOT FULL SOLUTION (Duplicate tuples, like (A, B) and (B, A) both gets listed)
 SELECT p1.name as Teacher1, p2.name as Teacher2 FROM (SELECT People.pid, cid, People.name FROM Teaches INNER JOIN People ON Teaches.pid = People.pid) as p1 INNER JOIN (SELECT People.pid, cid, People.name FROM Teaches INNER JOIN People ON Teaches.pid = People.pid) as p2 WHERE p1.pid < p2.pid AND p1.cid = p2.cid;
 #################################################################################################################
 #List each student together with the average grade for the student across all courses the student has taken
-#NB NOT FULL SOLUTION (counts duplicates of same course if the person has previously failed the course)
-SELECT name, AVG(grade) as average FROM People, Takes WHERE People.pid = Takes.pid GROUP BY People.pid;
+SELECT name, AVG(grade) as average FROM 
+	(SELECT name, People.pid, cid, max(`date`) as date FROM People, Takes WHERE People.pid = Takes.pid GROUP BY cid, pid) as p1 
+    INNER JOIN 
+	(SELECT People.pid, cid, `date`, grade FROM People, Takes WHERE People.pid = Takes.pid) as p2 
+    ON 
+    p1.pid = p2.pid AND p1.cid = p2.cid AND p1.date = p2.date GROUP BY p1.pid;
 #################################################################################################################
 #How many distinct average grades are there among students
-SELECT COUNT(DISTINCT average) as DistinctAverages FROM (SELECT AVG(grade) as average FROM People, Takes WHERE People.pid = Takes.pid GROUP BY People.pid) as averagegrades;
+SELECT COUNT(DISTINCT average) as DistinctAverages FROM (
+SELECT name, AVG(grade) as average FROM 
+	(SELECT name, People.pid, cid, max(`date`) as date FROM People, Takes WHERE People.pid = Takes.pid GROUP BY cid, pid) as p1 
+    INNER JOIN 
+	(SELECT People.pid, cid, `date`, grade FROM People, Takes WHERE People.pid = Takes.pid) as p2 
+    ON 
+    p1.pid = p2.pid AND p1.cid = p2.cid AND p1.date = p2.date GROUP BY p1.pid
+) as averagegrades;
+
 #################################################################################################################
